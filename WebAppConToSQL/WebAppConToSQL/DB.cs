a4680ebb620457c4283939ad80d7d85346cca566
babyfoot <ron0817@gmail.com>
2017-01-18 13:41:45 +0800

    新增程式

---
+++ WebAppConToSQL/WebAppConToSQL/DB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebAppConToSQL
+{
+    public class DB
+    {
+        public string ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True";
+
+        public void Inserts(String Name, String City, String Phone, String Mail)
+        {
+            string QueryString = "Insert into dbo.Contact (Name,City,Phone,Mail) Values ('" + Name + "','" + City + "','" + Phone + "','" + Mail + "')";
+            SqlConnection con = new SqlConnection(ConStr);
+            SqlCommand cmd = new SqlCommand(QueryString, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Delete()
+        {
+        }
+    }
+}
---
+++ WebAppConToSQL/WebAppConToSQL/WebForm1.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppConToSQL
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            string Name = TextBoxName.Text;
+            string City = TextBoxCity.Text;
+            string Phone = TextBoxPhone.Text;
+            string Mail = TextBoxMail.Text;
+
+            DB db1 = new DB();
+            db1.Inserts(Name, City, Phone, Mail);
+
+            GridView1.DataBind();
+        }
+    }
+}
